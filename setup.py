165cf8861e712c0b652170dd9cf209701dca0b3a
Ines Montani <ines@ines.io>
2019-11-01 19:08:40 +0100

    Fix single-file package

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
-# coding: utf8
-from __future__ import unicode_literals
-
+# coding: utf-8
 
 if __name__ == "__main__":
-    from setuptools import setup, find_packages
+    from setuptools import setup
 
-    setup(name="catalogue", packages=find_packages())
+    setup()
