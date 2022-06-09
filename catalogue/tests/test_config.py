441367ae3a8f8005ec4976a0479994b8a3457674
Raphael Mitsch <r.mitsch@outlook.com>
2022-06-09 13:55:54 +0200

    Simplify EXAMPLE_CONFIG, make less spaCy-specific.

--- catalogue/tests/test_config.py
+++ catalogue/tests/test_config.py
@@ -28,42 +28,20 @@ total_steps = 100000
 
 [pipeline]
 
-[pipeline.parser]
-name = "parser"
-factory = "parser"
+[pipeline.classifier]
+name = "classifier"
+factory = "classifier"
 
-[pipeline.parser.model]
-@layers = "spacy.ParserModel.v1"
+[pipeline.classifier.model]
+@layers = "ClassifierModel.v1"
 hidden_depth = 1
 hidden_width = 64
 token_vector_width = 128
 
-[pipeline.parser.model.tok2vec]
-@layers = "Tok2Vec.v1"
-width = ${pipeline.parser.model:token_vector_width}
+[pipeline.classifier.model.embedding]
+@layers = "Embedding.v1"
+width = ${pipeline.classifier.model:token_vector_width}
 
-[pipeline.parser.model.tok2vec.embed]
-@layers = "spacy.MultiFeatureHashEmbed.v1"
-width = ${pipeline.parser.model.tok2vec:width}
-
-[pipeline.parser.model.tok2vec.embed.hidden]
-@layers = "MLP.v1"
-depth = 1
-pieces = 3
-layer_norm = true
-outputs = ${pipeline.parser.model.tok2vec.embed:width}
-
-[pipeline.parser.model.tok2vec.encode]
-@layers = "spacy.MaxoutWindowEncoder.v1"
-depth = 4
-pieces = 3
-window_size = 1
-
-[pipeline.parser.model.lower]
-@layers = "spacy.ParserLower.v1"
-
-[pipeline.parser.model.upper]
-@layers = "thinc.Linear.v1"
 """
 
 OPTIMIZER_CFG = """
@@ -279,8 +257,8 @@ def test_read_config():
 
     assert cfg["optimizer"]["beta1"] == 0.9
     assert cfg["optimizer"]["learn_rate"]["initial_rate"] == 0.1
-    assert cfg["pipeline"]["parser"]["factory"] == "parser"
-    assert cfg["pipeline"]["parser"]["model"]["tok2vec"]["width"] == 128
+    assert cfg["pipeline"]["classifier"]["factory"] == "classifier"
+    assert cfg["pipeline"]["classifier"]["model"]["embedding"]["width"] == 128
 
 
 def test_optimizer_config():
