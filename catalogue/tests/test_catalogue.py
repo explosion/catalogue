0a78a86e1ec7f071ab3d2eb6d4f1d7090349f262
Raphael Mitsch <r.mitsch@outlook.com>
2022-07-12 17:52:05 +0200

    Amended setup install dependencies.

--- catalogue/tests/test_catalogue.py
+++ catalogue/tests/test_catalogue.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import catalogue
 try:  # Python 3.8
     import importlib.metadata as importlib_metadata
-except (ImportError, ModuleNotFoundError):
+except ImportError:
     import importlib_metadata  # type: ignore
 from .._importlib_metadata import backports
 from configparser import ConfigParser
